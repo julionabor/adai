8e2f4e8e37ca06af2b43aa96946a8a8c77069303
Julio Nabor <julionabor@gmail.com>
2023-03-31 19:34:00 +0100

    new features home

--- src/components/pages/Beneficios.js
+++ src/components/pages/Beneficios.js
@@ -4,7 +4,6 @@ import styles from './Beneficios.module.css'
 
 function Beneficios (){
     return (
-<<<<<<< HEAD
         <Container>
             <div className={styles.beneficios}>
                 <h1>Qual benefício em ser associado?</h1>
@@ -21,25 +20,6 @@ function Beneficios (){
             </div>
         </Container>
        
-=======
-        <div className={styles.carousel}>
-        <Container>
-            <h1>Qual benefício em ser associado?</h1>
-
-            <h2>Ao se associar ao ADAI, você poderá usufruir dos seguintes beneficios:</h2>
-            <ul>
-                <li>Treino Funcional: Terças e Quintas (20h as 21h)</li>
-                <li>Desconto de 10% nos seguintes espaços: 48 E PICO, HSR Auto e EcoWash Sao Joao da Madeira</li>
-                <li>Futebol semanal (Pelada) gratuita - (não associado paga 3€)</li>
-                <li>Rifas mensais com ofertas nos patrocinadores aderentes.</li>
-                <li>Poder participar gratuitamente do espaço comerciante.</li>
-                <li>Orientação em como e onde tirar determinadas documentações.</li>
-            </ul>
-
-
-        </Container>
-        </div>
->>>>>>> 26262c2e263151370f7ccdde6ac8ea7588d40b7d
     )
 }
 export default Beneficios
